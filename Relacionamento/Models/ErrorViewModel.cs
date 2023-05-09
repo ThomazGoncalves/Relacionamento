60a5f8be3f507cbf90b0036f119eb6ae076d11ee
ThomazGoncalves <thomazass100@gmail.com>
2023-05-09 20:44:44 -0300

    Adicionar arquivos de projeto.

---
+++ Relacionamento/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Relacionamento.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Relacionamento/Models/Model.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+    public string DbPath { get; }
+
+    public BloggingContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "blogging.db");
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class Blog
+{
+    public string Name { get; set; }
+    public virtual Uri SiteUri { get; set; }
+
+    public ICollection<Post> Posts { get; }
+}
+
+public class Post
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateOnly PublishedOn { get; set; }
+    public bool Archived { get; set; }
+
+    public Blog Blog { get; set; }
+}
